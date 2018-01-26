8116fcf3ef92d252269c828bca8fe827c8d417f7
Steven Gan <steven.gan@shareworks.cn>
2018-01-26 17:18:38 +0800

    installation script for puppet

---
+++ puppet-server/installation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# puppet server
+wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
+sudo dpkg -i puppetlabs-release-trusty.deb
+sudo apt-get update
+
+sudo apt-get install puppetserver
