fd04e44af2f8d9d734b12c329149361bc233d21b
甘红波 <281046367@qq.com>
2018-02-05 09:15:25 +0800

    puppet-server

--- puppet-server/installation.sh
+++ puppet-server/installation.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-# puppet server
-wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
-sudo dpkg -i puppetlabs-release-trusty.deb
-sudo apt-get update
+set -e
 
-sudo apt-get install puppetserver
+REPO_DEB_URL="https://apt.puppetlabs.com/puppetlabs-release-trusty.deb"
+
+#--------------------------------------------------------------------
+# NO TUNABLES BELOW THIS POINT
+#--------------------------------------------------------------------
+if [ "$EUID" -ne "0" ]; then
+  echo "This script must be run as root." >&2
+  exit 1
+fi
+
+# Install the PuppetLabs repo
+echo "Configuring PuppetLabs repo..."
+repo_deb_path=$(mktemp)
+wget --output-document=${repo_deb_path} ${REPO_DEB_URL} 2>/dev/null
+dpkg -i ${repo_deb_path} >/dev/null
+apt-get update >/dev/null
+
+# Install Puppet Server
+echo "Installing Puppet..."
+
+apt-get install -y puppetserver >/dev/null
+
+echo "Puppet installed!"
